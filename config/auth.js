d81cbe2680696931fd1ceb6914b3652af127cf30
gschnall <gabriel.schnall@gmail.com>
2016-03-17 19:21:23 -0700

    Modified facebook settings

--- config/auth.js
+++ config/auth.js
@@ -9,7 +9,7 @@ module.exports = {
   'facebook': {
     'clientID': process.env.FACEBOOK_APP_ID,
     'clientSecret': process.env.FACEBOOK_APP_SECRET,
-    'callbackURL': 'http://localhost:3000/auth/facebook/callback',
+    'callbackURL': 'https://ventcal.herokuapp.com/auth/facebook/callback',
     'profileFields': ['emails', 'displayName']
   }
 }
